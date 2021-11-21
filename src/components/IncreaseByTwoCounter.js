d1704d72a6fe61605ce07bf4779f1ba191b5abe2
ibra2626 <58219232+ibra2626@users.noreply.github.com>
2021-11-21 19:02:49 +0300

    Revert "Revert "import redux reducer/store/actiıns""
    This reverts commit ea17996fb85f1557e91738c51dec8e5386aef6da.

---
+++ src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Increasebytwocounter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Increasebytwocounter;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import configureStore from './redux/reducers/configureStore';
 import App from './App';
 
+
+const store = configureStore();
+
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
