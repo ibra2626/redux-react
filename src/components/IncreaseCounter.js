da0df6ff72d0284f739db00a6a5926b1f4c18262
ibra2626 <58219232+ibra2626@users.noreply.github.com>
2021-11-21 19:12:29 +0300

    create increase counter

--- src/components/IncreaseCounter.js
+++ src/components/IncreaseCounter.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {increaseCounter} from '../redux/actions/counterActions'
 class Increasecounter extends Component {
     render() {
         return (
             <div>
-                
+                <button onClick={e => {this.props.dispatch(increaseCounter())}}>
+                    1 arttır
+                </button>
             </div>
         );
     }
 }
 
-export default Increasecounter;
+function mapDispartchProps(dispatch) {
+    return {actions : bindActionCreators(increaseCounter,dispatch)}
+}
+export default connect(mapDispartchProps)(Increasecounter); 
